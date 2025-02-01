6489be32bff67cccc419590a08b15d0754489319
Aamir Khan <sengoku264@gmail.com>
2025-02-01 22:31:07 +0530

    lots

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+
+
+
+const addProduct = async (req,res) => {
+
+}
+
+const listProduct = async (req,res) => {
+    
+}
+
+const removeProduct = async (req,res) => {
+    
+}
+
+const singleProduct = async (req,res) => {
+    
+}
+
+export {listProduct,addProduct,removeProduct,singleProduct}
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,2 @@
+import express from 'express'
+import { listProduct,addProduct,singleProduct,removeProduct } from '../controllers/productController.js'
